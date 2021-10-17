27e78cf322f41e5b545f7b0e7c61311beca94620
Tish Faroul <lfaroul14@gmail.com>
2021-10-16 23:20:13 -0400

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :shows
+
+  def actors_list
+    self.characters.map do |c|
+        c.actor.full_name
+    end
+  end 
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |column|
+      column.string :name
+      column.integer :actor_id
+      column.integer :show_id
+    end 
   end
 end
